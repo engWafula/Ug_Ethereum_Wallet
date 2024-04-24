0daff7edbb3822a62b362723ba163f477fe6cfb6
engWafula <wafulaallan5@gmail.com>
2024-04-24 16:44:13 +0300

    dndjd

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,9 +6,9 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   solidity: "0.8.0",
   networks:{
-    ropsten:{
-      url:"https://eth-rinkeby.alchemyapi.io/v2/XYSWRcClLboUqcoSEd9uLNSmQl0rEaNf",
-      accounts:["eea92db9f48d1c068d300f1906f25521124834220dd5bf13b8da2fb8422fa939"]
+    sepolia:{
+      url:"https://eth-sepolia.g.alchemy.com/v2/znZ_CskoANU8i_HOyEDvpIMaWGYg0518",
+      accounts:["73553193e47d917206720276e00a01f30a0fa6c21675539c3a2a1ca122dc7bdc"]
     },
   },
 };
